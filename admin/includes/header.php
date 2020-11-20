c241b29f920d17b6d137844b04de6da51852df92
varundemo <74758588+varundemo@users.noreply.github.com>
2020-11-20 20:07:09 +0530

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -27,39 +27,39 @@
 		<div class="sidebar-sticky">
 			<ul class="nav flex-column">
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == dashboard){ echo "active"; } ?>" href="dashboard.php"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'dashboard'){ echo "active"; } ?>" href="dashboard.php"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
 				</li>
 
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == work){ echo "active"; } ?>" href="work.php"><i class="fab fa-accessible-icon"></i>Work Order</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'work'){ echo "active"; } ?>" href="work.php"><i class="fab fa-accessible-icon"></i>Work Order</a>
 				</li>
 
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == requests){ echo "active"; } ?>" href="Request.php"><i class="fas fa-align-center"></i>Requests</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'requests'){ echo "active"; } ?>" href="Request.php"><i class="fas fa-align-center"></i>Requests</a>
 				</li>
 
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == assets){ echo "active"; } ?>" href="assets.php"><i class="fas fa-database"></i>Assets</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'assets'){ echo "active"; } ?>" href="assets.php"><i class="fas fa-database"></i>Assets</a>
 				</li>
 
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == technician){ echo "active"; } ?>" href="technician.php"><i class="fab fa-teamspeak"></i>Technician</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'technician'){ echo "active"; } ?>" href="technician.php"><i class="fab fa-teamspeak"></i>Technician</a>
 				</li>
 
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == requester){ echo "active"; } ?>" href="Requester.php"><i class="fas fa-user"></i>Requester</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'requester'){ echo "active"; } ?>" href="Requester.php"><i class="fas fa-user"></i>Requester</a>
 				</li>
 
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == sell_report){ echo "active"; } ?>" href="sellproductreport.php"><i class="fas fa-table"></i>Sell Report</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'sell_report'){ echo "active"; } ?>" href="sellproductreport.php"><i class="fas fa-table"></i>Sell Report</a>
 				</li>
 
 				<li>
-					<a class="nav-link text-dark <?php if(PAGE == work_report){ echo "active"; } ?>" href="workreport.php"><i class="fas fa-table"></i>Work Report</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'work_report'){ echo "active"; } ?>" href="workreport.php"><i class="fas fa-table"></i>Work Report</a>
 				</li>
 
 				<li class="nav-item">
-					<a class="nav-link text-dark <?php if(PAGE == change_pass){ echo "active"; } ?>" href="changepass.php"><i class="fas fa-key"></i>Change Password</a>
+					<a class="nav-link text-dark <?php if(PAGE == 'change_pass'){ echo "active"; } ?>" href="changepass.php"><i class="fas fa-key"></i>Change Password</a>
 				</li>
 
 				<li class="nav-item">
